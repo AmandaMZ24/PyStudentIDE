7fa00b7a125f51f7f931381adb58df29246e949f
Mishelle <mishelleorellanajimenez@gmail.com>
2026-06-01 18:42:39 -0600

    Listo todo

--- src/PyStudentIDE.UI/App.xaml.cs
+++ src/PyStudentIDE.UI/App.xaml.cs
@@ -5,7 +5,7 @@ namespace PyStudentIDE.UI;
 
 public partial class App : System.Windows.Application
 {
-    public static ApiClient Api { get; } = new(new HttpClient { BaseAddress = new Uri("https://localhost:5001/") });
+    public static ApiClient Api { get; } = new(new HttpClient { BaseAddress = new Uri("http://localhost:5000/") });
     public static ScriptManager ScriptMgr { get; } = new();
     public static int CurrentUserId { get; set; }
     public static string CurrentUserName { get; set; } = string.Empty;
